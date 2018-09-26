78704f9dab30afd7463501a6bf983b67e82f0472
Raden Adhyaksa <raksa69@gmail.com>
2018-09-26 23:51:14 +0700

    Create Project

---
+++ app/src/main/java/com/example/aksaden/codescanner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aksaden.codescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
